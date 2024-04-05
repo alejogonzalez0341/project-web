a204e3357757e63558889e1c4a7c6775a0cdea66
Gonzalez <alejogonzalez0341@gmail.com>
2024-04-04 19:40:06 -0500

    borrando gitignore

---
+++ admin/db.py
@@ -0,0 +1,10 @@
+admin_user_db= {
+    "gregory":{
+        "username": "gregory",
+        "fullname": "administrador",
+        "email": "admin@gmail.com",
+        "password": "$pbkdf2-sha256$29000$F0JIqTXmXKtVqnWu9X4vpQ$RowiT9RDvZ1ollOuWGkJd/E0v/wDfcDQSJGxR9Es5pA",
+        "disabled": False
+    },
+}
+
